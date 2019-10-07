3ac53ed566dac1ea72d762b5bb42334630ffff03
kor0p <viktorua25.03@gmail.com>
2019-10-07 09:50:02 +0300

    Add lab5

---
+++ admin.js
@@ -0,0 +1,8 @@
+var input = document.querySelector('input');
+var preview = document.querySelector('.col-xxl img');
+input.addEventListener('change', function() {
+    var file = input.files[0];
+    console.log(file);
+    console.log(window);
+    preview.src = window.URL.createObjectURL(file);
+});
